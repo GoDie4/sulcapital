868a1ef226e4eb3dbacf81f47c4f1c990a9cee4c
GoDie4 <anthony10.reyes10@gmail.com>
2025-06-04 13:02:20 -0500

    Fix:Cors-domain6

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,22 +3,25 @@ import cors from "cors";
 import path from "path";
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:3000",
-  "http://localhost:4000",
-  "https://sulcapital.exportando.online",
-  "https://www.sulcapital.exportando.online",
-];
 
-app.options(
-  "*",
-  cors({
-    origin: allowedOrigins,
+app.options("*", cors({
+    origin: function (origin, callback) {
+      const allowed = [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:3000",
+        "http://localhost:4000",
+        "https://sulcapital.exportando.online",
+        "https://www.sulcapital.exportando.online",
+      ];
+      if (!origin || allowed.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-  })
-);
+  }));
 
 app.use("/public", express.static(path.resolve("public")));
 
