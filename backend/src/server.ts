e7e0f8f4c81a599d67d656906bae59bcebf97bc0
GoDie4 <anthony10.reyes10@gmail.com>
2025-06-04 13:29:35 -0500

    Fix:Cors-domain9

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,25 +3,14 @@ import cors from "cors";
 import path from "path";
 
 const app = express();
+
 app.use(
   cors({
     origin: "https://sulcapital.exportando.online",
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
-// Y también para OPTIONS, aunque express-cors ya lo cubre:
-app.options(
-  "*",
-  cors({
-    origin: "https://sulcapital.exportando.online",
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  })
-);
 app.use("/public", express.static(path.resolve("public")));
 
 app.use(express.json());
